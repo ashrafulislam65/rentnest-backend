6bba1da41f41623b1ef6891166782097a5fe449a
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2026-07-09 08:43:52 +0600

    rental controller issue partialy fixed

--- src/controllers/rental.controller.ts
+++ src/controllers/rental.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
-import { PrismaClient } from '@prisma/client';
+import prisma from '../config/prisma.js';
 
-const prisma = new PrismaClient();
 
 export const submitRentalRequest = async (req: Request, res: Response, next: NextFunction) => {
   try {
