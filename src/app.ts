e9ded6a0e221501df46eb39fcace03398a6876b1
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2026-07-10 12:18:05 +0600

    little change in readme

--- src/app.ts
+++ src/app.ts
@@ -15,12 +15,12 @@ const app: Application = express();
 app.use(cors());
 app.use(express.json());
 
-// ১. Base Route
+
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ success: true, message: "Welcome to RentNest API" });
 });
 
-// ২. Application Routes (সব মেইন রাউট ৪০৪ এবং গ্লোবাল এরর হ্যান্ডলারের ওপরে থাকবে)
+
 app.use('/api/auth', authRoutes);
 app.use('/api', publicPropertyRoutes);
 app.use('/api/landlord', landlordRoutes);
@@ -29,12 +29,12 @@ app.use('/api/payments', paymentRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/reviews', reviewRoutes);
 
-// ৩. 404 Route Handling (রাউট খুঁজে না পেলে এটি ট্রিগার হবে)
+
 app.use((req: Request, res: Response) => {
   res.status(404).json({ success: false, message: "Route Not Found", errorDetails: {} });
 });
 
-// ৪. Global Error Handler (একদম শেষে থাকবে যাতে যেকোনো জায়গা থেকে next(err) দিলে এখানে আসে)
+
 app.use(globalErrorHandler);
 
 export default app;
